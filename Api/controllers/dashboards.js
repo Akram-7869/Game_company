b2c67e3e0dc735155d2155102b8c4b4254ec49d5
kamlesh <kamlesh@example.co0m>
2022-06-27 14:31:50 +0530

    fix

--- Api/controllers/dashboards.js
+++ Api/controllers/dashboards.js
@@ -163,8 +163,8 @@ exports.getFilterDashboard = asyncHandler(async (req, res, next) => {
   row['livePlayers'] = req.io.engine.clientsCount;
   let payout = await payoutTotal();
   //console.log('payoutTotal', payout);
-  row['withdrawRequest'] = payout['totalCount'];
-  row['withdrawTotal'] = payout['totalWithdraw'];
+  row['withdrawRequest'] = await Transaction.countDocuments({ logType: 'withdraw', paymentStatus: 'PROCESSING' });
+  row['withdrawTotal'] = 0;
 
   row['supportRequest'] = await Ticket.countDocuments();
   row['gameCount'] = await PlayerGame.countDocuments();
