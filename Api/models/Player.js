49a535d9787b2e3118523ac8f0822cd2a45fe79f
Your Name <you@example.com>
2022-09-11 00:23:54 +0530

    fix

--- Api/models/Player.js
+++ Api/models/Player.js
@@ -45,7 +45,7 @@ const PlayerSchema = new mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    required: [true, 'Please add an email'],
+    required: [true, 'try again'],
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'try again'
