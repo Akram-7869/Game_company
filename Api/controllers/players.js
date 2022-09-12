cd8daa0e62be994dbddf55f702bfc4043d975f29
kamlesh <kamlesh@example.com>
2022-09-12 06:30:10 +0530

    fix

--- Api/controllers/players.js
+++ Api/controllers/players.js
@@ -1334,7 +1334,7 @@ exports.saveLeaderBoard = asyncHandler(async (req, res, next) => {
     const commision = betAmout - winAmount;
     Dashboard.totalIncome(betAmout, winAmount, commision);
     updatedData['isBoat'] = true;
-    updatedData['plauerId'] = looserPlayer.plauerId;
+    updatedData['playerId'] = looserPlayer.userId;
     updatedData['gameStatus'] = 'lost';
   }
 
