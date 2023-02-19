efbeab0e0dc0928287296288209fb3714be07507
kamlesh <kamlesh@example.com>
2023-02-19 12:06:32 +0530

    test

--- Api/server.js
+++ Api/server.js
@@ -166,7 +166,7 @@ io.on('connection', socket => {
     } else {
       roomName = makeid(5);
       publicRoom[lobbyId] = { roomName, playerCount: 0, played: false }
-      state[roomName] = { 'created': Date.now() + 600000, players: [], 'betList': [], gameData: {}, WinList: [] };
+      state[roomName] = { 'created': Date.now() + 600000, players: [], betList: [], gameData: {}, WinList: [] };
       console.log('create-room-', roomName);
       //   await PlayerGame.create({ playerId: userId, 'gameId': roomName, 'tournamentId': lobbyId, playerCount: 1, gameData: {}, WinList: {} });
     }
