403c132663b39d82a03a7448b322700a83640ed9
kamlesh <kamlesh@example.com>
2023-02-19 14:55:41 +0530

    test

--- Api/server.js
+++ Api/server.js
@@ -348,10 +348,11 @@ io.on('connection', socket => {
 
     let { room, betNo, amount } = d; //JSON.parse(d);
     console.log('setBetData', d);
-    console.log('a----old', amount, '---', state[room]['betList'][foundIndex]['amount'])
+
 
     if (state[room]) {
       var foundIndex = state[room]['betList'].findIndex(x => x.id == betNo);
+      console.log('a----old', foundIndex, '===', amount, '---', state[room]['betList'][foundIndex]['amount'])
       state[room]['betList'][foundIndex]['amount'] = parseInt(amount) + parseInt(state[room]['betList'][foundIndex]['amount']);
     }
   });
