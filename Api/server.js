8d6d887625ad7814edc1223ef88bb1332588cd7c
kamlesh <kamleshpawar2000@yahoo.com>
2024-03-19 12:36:06 +0530

    test

--- Api/server.js
+++ Api/server.js
@@ -51,7 +51,7 @@ const Setting = require('./models/Setting');
 const PlayerGame = require('./models/PlayerGame');
 
 // Body parser
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.use(express.urlencoded({
   extended: true
