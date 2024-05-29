3acd78b0e3f1179dd0bd778e8125079245a54415
kamlesh <kamleshpawar2000@yahoo.com>
2024-05-29 12:03:58 +0530

    test

--- Api/models/Player.js
+++ Api/models/Player.js
@@ -45,7 +45,6 @@ const PlayerSchema = new mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    required: [true, 'try again'],
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'try again'
