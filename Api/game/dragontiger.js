fd924af3b503a0fa46339071d3c7ba34ce49d39d
kamlesh <kamleshpawar2000@yahoo.com>
2024-07-25 11:59:24 +0530

    test

--- Api/game/dragontiger.js
+++ Api/game/dragontiger.js
@@ -1,8 +1,31 @@
 const Timer = require("./Timer");
 
 class DragonTigerGame {
+    static io;
+    static staticDeck = [
+        // Clubs (0-12)
+        { cardNo: 2, color: 1 }, { cardNo: 3, color: 1 }, { cardNo: 4, color: 1 }, { cardNo: 5, color: 1 },
+        { cardNo: 6, color: 1 }, { cardNo: 7, color: 1 }, { cardNo: 8, color: 1 }, { cardNo: 9, color: 1 },
+        { cardNo: 10, color: 1 }, { cardNo: 11, color: 1 }, { cardNo: 12, color: 1 }, { cardNo: 13, color: 1 },
+        { cardNo: 14, color: 1 },
+        // Diamonds (13-25)
+        { cardNo: 2, color: 2 }, { cardNo: 3, color: 2 }, { cardNo: 4, color: 2 }, { cardNo: 5, color: 2 },
+        { cardNo: 6, color: 2 }, { cardNo: 7, color: 2 }, { cardNo: 8, color: 2 }, { cardNo: 9, color: 2 },
+        { cardNo: 10, color: 2 }, { cardNo: 11, color: 2 }, { cardNo: 12, color: 2 }, { cardNo: 13, color: 2 },
+        { cardNo: 14, color: 2 },
+        // Spades (26-38)
+        { cardNo: 2, color: 3 }, { cardNo: 3, color: 3 }, { cardNo: 4, color: 3 }, { cardNo: 5, color: 3 },
+        { cardNo: 6, color: 3 }, { cardNo: 7, color: 3 }, { cardNo: 8, color: 3 }, { cardNo: 9, color: 3 },
+        { cardNo: 10, color: 3 }, { cardNo: 11, color: 3 }, { cardNo: 12, color: 3 }, { cardNo: 13, color: 3 },
+        { cardNo: 14, color: 3 },
+        // Hearts (39-51)
+        { cardNo: 2, color: 4 }, { cardNo: 3, color: 4 }, { cardNo: 4, color: 4 }, { cardNo: 5, color: 4 },
+        { cardNo: 6, color: 4 }, { cardNo: 7, color: 4 }, { cardNo: 8, color: 4 }, { cardNo: 9, color: 4 },
+        { cardNo: 10, color: 4 }, { cardNo: 11, color: 4 }, { cardNo: 12, color: 4 }, { cardNo: 13, color: 4 },
+        { cardNo: 14, color: 4 }
+    ];
     constructor(roomName, io) {
-        this.io = io;
+        DragonTigerGame.io = io;
         this.roomName = roomName;
         this.currentPhase = 'betting';
         this.winList = [1, 2, 3, 3, 1, 2, 1, 3];
@@ -14,28 +37,7 @@ class DragonTigerGame {
         this.pauseTime = 9; // 5 seconds
         this.players = new Set();
         this.timerRunning = false; // To track if the timer is running
-        this.staticDeck = [
-            // Clubs (0-12)
-            { cardNo: 2, color: 1 }, { cardNo: 3, color: 1 }, { cardNo: 4, color: 1 }, { cardNo: 5, color: 1 },
-            { cardNo: 6, color: 1 }, { cardNo: 7, color: 1 }, { cardNo: 8, color: 1 }, { cardNo: 9, color: 1 },
-            { cardNo: 10, color: 1 }, { cardNo: 11, color: 1 }, { cardNo: 12, color: 1 }, { cardNo: 13, color: 1 },
-            { cardNo: 14, color: 1 },
-            // Diamonds (13-25)
-            { cardNo: 2, color: 2 }, { cardNo: 3, color: 2 }, { cardNo: 4, color: 2 }, { cardNo: 5, color: 2 },
-            { cardNo: 6, color: 2 }, { cardNo: 7, color: 2 }, { cardNo: 8, color: 2 }, { cardNo: 9, color: 2 },
-            { cardNo: 10, color: 2 }, { cardNo: 11, color: 2 }, { cardNo: 12, color: 2 }, { cardNo: 13, color: 2 },
-            { cardNo: 14, color: 2 },
-            // Spades (26-38)
-            { cardNo: 2, color: 3 }, { cardNo: 3, color: 3 }, { cardNo: 4, color: 3 }, { cardNo: 5, color: 3 },
-            { cardNo: 6, color: 3 }, { cardNo: 7, color: 3 }, { cardNo: 8, color: 3 }, { cardNo: 9, color: 3 },
-            { cardNo: 10, color: 3 }, { cardNo: 11, color: 3 }, { cardNo: 12, color: 3 }, { cardNo: 13, color: 3 },
-            { cardNo: 14, color: 3 },
-            // Hearts (39-51)
-            { cardNo: 2, color: 4 }, { cardNo: 3, color: 4 }, { cardNo: 4, color: 4 }, { cardNo: 5, color: 4 },
-            { cardNo: 6, color: 4 }, { cardNo: 7, color: 4 }, { cardNo: 8, color: 4 }, { cardNo: 9, color: 4 },
-            { cardNo: 10, color: 4 }, { cardNo: 11, color: 4 }, { cardNo: 12, color: 4 }, { cardNo: 13, color: 4 },
-            { cardNo: 14, color: 4 }
-        ];
+
 
     }
 
@@ -45,26 +47,46 @@ class DragonTigerGame {
         const tigerPayout = this.tigerBet * 2;
         const tiePayout = this.tieBet * 10;
 
-        if (dragonPayout <= tigerPayout && dragonPayout <= tiePayout) {
-            // Dragon should win
-            do {
-                dragonCardIndex = Math.floor(Math.random() * 52);
-                tigerCardIndex = Math.floor(Math.random() * 52);
-            } while (this.staticDeck[dragonCardIndex].cardNo <= this.staticDeck[tigerCardIndex].cardNo);
-            winner = 1;
-        } else if (tigerPayout <= dragonPayout && tigerPayout <= tiePayout) {
-            do {
-                dragonCardIndex = Math.floor(Math.random() * 52);
-                tigerCardIndex = Math.floor(Math.random() * 52);
-            } while (this.staticDeck[tigerCardIndex].cardNo <= this.staticDeck[dragonCardIndex].cardNo);
-            winner = 2;
+        if (dragonPayout == 0 && tigerPayout == 0 && tiePayout == 0) {
+            winner = Math.floor(Math.random() * 3) + 1;
+
         } else {
-            do {
-                dragonCardIndex = Math.floor(Math.random() * 52);
-                tigerCardIndex = Math.floor(Math.random() * 52);
-            } while (this.staticDeck[dragonCardIndex].cardNo !== this.staticDeck[tigerCardIndex].cardNo);
-            winner = 3;
+            if (dragonPayout <= tigerPayout && dragonPayout <= tiePayout) {
+                winner = 1;//'dragon';
+            } else if (tigerPayout <= dragonPayout && tigerPayout <= tiePayout) {
+                winner = 2;//'tiger';
+            } else {
+                winner = 3;//'tie';
+            }
         }
+
+        switch (winner) {
+            case 1:
+                do {
+                    dragonCardIndex = Math.floor(Math.random() * 52);
+                    tigerCardIndex = Math.floor(Math.random() * 52);
+                } while (DragonTigerGame.staticDeck[dragonCardIndex].cardNo <= DragonTigerGame.staticDeck[tigerCardIndex].cardNo);
+
+                break;
+
+            case 2:
+                do {
+                    dragonCardIndex = Math.floor(Math.random() * 52);
+                    tigerCardIndex = Math.floor(Math.random() * 52);
+                } while (DragonTigerGame.staticDeck[tigerCardIndex].cardNo <= DragonTigerGame.staticDeck[dragonCardIndex].cardNo);
+                break;
+            case 3:
+                do {
+                    dragonCardIndex = Math.floor(Math.random() * 52);
+                    tigerCardIndex = Math.floor(Math.random() * 52);
+                } while (DragonTigerGame.staticDeck[dragonCardIndex].cardNo !== DragonTigerGame.staticDeck[tigerCardIndex].cardNo);
+
+                break;
+
+            default:
+                break;
+        }
+
         return { dragonCardIndex, tigerCardIndex, winner };
     }
 
@@ -76,12 +98,12 @@ class DragonTigerGame {
         this.dragonBet = 0;
         this.tigerBet = 0;
         this.tieBet = 0
-        this.io.to(this.roomName).emit('OnTimerStart', { phase: 'betting', winList: this.winList,betting_remaing: this.bettingTimer?.remaining,});
+        DragonTigerGame.io.to(this.roomName).emit('OnTimerStart', { phase: 'betting', winList: this.winList, betting_remaing: this.bettingTimer?.remaining, });
         console.log(`Betting phase started in room: ${this.roomName}`);
 
         this.bettingTimer = new Timer(this.bettingTime, (remaining) => {
             console.log(remaining);
-            this.io.to(this.roomName).emit('betting_tick', { remainingTime: remaining });
+            DragonTigerGame.io.to(this.roomName).emit('betting_tick', { remainingTime: remaining });
         }, () => {
             this.startPausePhase();
         });
@@ -91,14 +113,14 @@ class DragonTigerGame {
 
     startPausePhase() {
         this.currentPhase = 'pause';
-        this.io.to(this.roomName).emit('OnTimeUp', { phase: 'pause' });
+        DragonTigerGame.io.to(this.roomName).emit('OnTimeUp', { phase: 'pause' });
         console.log(`Pause phase started in room: ${this.roomName}`);
         const { dragonCardIndex, tigerCardIndex, winner } = this.selectWinningCards();
 
         this.winList.push(winner);
         this.winList.shift();
         // Emit the result to all clients immediately
-        this.io.to(this.roomName).emit('OnWinNo', {
+        DragonTigerGame.io.to(this.roomName).emit('OnWinNo', {
             dragonCardIndex,
             tigerCardIndex,
             winner
@@ -106,14 +128,14 @@ class DragonTigerGame {
 
         this.pauseTimer = new Timer(this.pauseTime, (remaining) => {
             console.log(remaining);
-            if(remaining == 2){
+            if (remaining == 2) {
                 console.log('reseting');
-                this.io.to(this.roomName).emit('OnReset', { phase: 'reset' });
+                DragonTigerGame.io.to(this.roomName).emit('OnReset', { phase: 'reset' });
 
             }
-            //  this.io.to(this.roomName).emit('pause_tick', { remainingTime: remaining });
+            //  DragonTigerGame.io.to(this.roomName).emit('pause_tick', { remainingTime: remaining });
         }, () => {
-             this.resetTimers();
+            this.resetTimers();
         });
 
         this.pauseTimer.startTimer();
@@ -153,7 +175,7 @@ class DragonTigerGame {
 
             }
 
-            this.io.to(this.roomName).emit('onBetPlaced', d);
+            DragonTigerGame.io.to(this.roomName).emit('onBetPlaced', d);
 
         });
     }
@@ -163,7 +185,7 @@ class DragonTigerGame {
         if (this.currentPhase === 'betting') {
             this.players.add(socket.id);
             socket.join(this.roomName);
-            this.io.to(this.roomName).emit('player_joined', { id: socket.id });
+            DragonTigerGame.io.to(this.roomName).emit('player_joined', { id: socket.id });
             console.log(`Player ${socket.id} joined room ${this.roomName}`);
         } else {
             socket.emit('error', 'Can only join during betting phase.');
@@ -173,12 +195,12 @@ class DragonTigerGame {
     removePlayer(socket) {
         this.players.delete(socket.id);
         socket.leave(this.roomName);
-        this.io.to(this.roomName).emit('player_left', { id: socket.id });
+        DragonTigerGame.io.to(this.roomName).emit('player_left', { id: socket.id });
         console.log(`Player ${socket.id} left room ${this.roomName}`);
     }
     syncPlayer(socket, player) {
         // Send current game state to the player
-        this.io.to(socket.id).emit('OnCurrentTimer', {
+        DragonTigerGame.io.to(socket.id).emit('OnCurrentTimer', {
             gameType: 'DragonTiger',
             room: this.roomName,
             currentPhase: this.currentPhase,
@@ -219,19 +241,19 @@ class DragonTigerGame {
             }
         });
     }
-    OnCurrentStatus(socket){
+    OnCurrentStatus(socket) {
         socket.on('OnCurrentStatus', (d) => {
-        this.io.to(socket.id).emit('OnCurrentStatus', {
-            gameType: 'DragonTiger',
-            room: this.roomName,
-            currentPhase: this.currentPhase,
-            total_players: this.players.size,
-            betting_remaing: this.bettingTimer?.remaining,
-            pause_remaing: this.pauseTimer?.remaining,
-            winList: this.winList
+            DragonTigerGame.io.to(socket.id).emit('OnCurrentStatus', {
+                gameType: 'DragonTiger',
+                room: this.roomName,
+                currentPhase: this.currentPhase,
+                total_players: this.players.size,
+                betting_remaing: this.bettingTimer?.remaining,
+                pause_remaing: this.pauseTimer?.remaining,
+                winList: this.winList
 
+            });
         });
-    });
     }
 }
 
