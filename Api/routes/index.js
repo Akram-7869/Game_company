c1558e942d2ab446b23c76ade51b242bf0534ae6
Ankit <ankit.thapliyal522@gmail.com>
2024-11-08 12:34:29 +0530

    wallpost

--- Api/routes/index.js
+++ Api/routes/index.js
@@ -28,7 +28,7 @@ const influencers = require('./influencers');
 const commission = require('./commission');
 
 const { state} = require('../utils/JoinRoom');
-const WallPost = require('../models/WallPost');
+const wallPost = require('./wallPost');
 
 
 
@@ -49,7 +49,7 @@ router.use('/api/v1/dashboards', dashboards);
 router.use('/api/v1/tournaments', tournaments);
 router.use('/api/v1/coupon', coupon);
 router.use('/api/v1/polls', polls);
-router.use('/api/v1/wallPost', WallPost);
+router.use('/api/v1/wallPost', wallPost);
 router.use('/api/v1/gamemanager', gamemanager);
 router.use('/api/v1/bannertext', bannertext);
 router.use('/api/v1/posts', posts);
